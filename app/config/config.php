dfcd1c45f14072f82594b5f0b3253902e2eb4cfc
unknown <no17615@hotmail.com>
2018-06-22 14:32:03 +0700

    add path nmap

--- app/config/config.php
+++ app/config/config.php
@@ -24,6 +24,9 @@ return new \Phalcon\Config([
         'pluginsDir'     => APP_PATH . '/plugins/',
         'libraryDir'     => APP_PATH . '/library/',
         'cacheDir'       => BASE_PATH . '/cache/',
+        //manul add by creator
+        'nmap'           => APP_PATH . '/library/nmap/', 
+
 
         // This allows the baseUri to be understand project paths that are not in the root directory
         // of the webpspace.  This will break if the public/index.php entry point is moved or
